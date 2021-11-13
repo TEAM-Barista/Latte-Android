3c77b18baf093e7ee396493a11f042a50f0d1cda
juhyang <ljh28891004@gmail.com>
2021-11-14 05:34:59 +0900

    master : feat(Interview) : 기본 API 동작 완료

--- app/src/main/java/com/barista/latte/interview/viewmodels/InterviewViewModel.kt
+++ app/src/main/java/com/barista/latte/interview/viewmodels/InterviewViewModel.kt
@@ -3,32 +3,53 @@ package com.barista.latte.interview.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.barista.latte.models.auth.UserRepository
 import com.barista.latte.models.interview.Interview
 import com.barista.latte.models.interview.InterviewRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class InterviewViewModel @Inject constructor(val interviewRepository : InterviewRepository) : ViewModel() {
-    private val _newInterviewList : MutableLiveData<List<Interview>> = MutableLiveData()
-    val newInterviewList : LiveData<List<Interview>> get() = _newInterviewList
+class InterviewViewModel @Inject constructor(private val interviewRepository: InterviewRepository, private val userRepository: UserRepository) : ViewModel() {
+    private val _newInterviewList: MutableLiveData<List<Interview>> = MutableLiveData()
+    val newInterviewList: LiveData<List<Interview>> get() = _newInterviewList
 
-    private val _priorInterviewList : MutableLiveData<List<Interview>> = MutableLiveData()
-    val priorInterviewList : LiveData<List<Interview>> get() = _priorInterviewList
+    private val _priorInterviewList: MutableLiveData<List<Interview>> = MutableLiveData()
+    val priorInterviewList: LiveData<List<Interview>> get() = _priorInterviewList
 
 
     fun loadData() {
         getNewInterviewList()
-        getPriorInterviewList()
-        val newInterviewList = ArrayList<Interview>()
-
-        _newInterviewList.value = newInterviewList
-
-        val priorInterviewList = ArrayList<Interview>()
-
-        _priorInterviewList.value = priorInterviewList
+//        getPriorInterviewList() // TODO : 나중에 API 변경되면 수정 필요
     }
 
-    fun getNewInterviewList() {}
+    fun getNewInterviewList() {
+        viewModelScope.launch {
+            val response = interviewRepository.getInterviewListRecent()
+            if (response.isSuccessful) {
+                val interviewList = response.body() ?: return@launch
+                if (interviewList.isNotEmpty()) {
+                    val newInterviewList: MutableList<Interview> = mutableListOf(interviewList[0])
+                    _newInterviewList.value = newInterviewList
+                }
+
+                if (interviewList.size > 1) {
+                    val priorInterviewList: MutableList<Interview> = mutableListOf()
+                    priorInterviewList.addAll(interviewList.subList(1, interviewList.size))
+
+                    _priorInterviewList.value = priorInterviewList
+                }
+            } else {
+                if (response.code() == 401) {
+                    userRepository.refreshTokenWithServer {
+                        getNewInterviewList()
+                    }
+                }
+            }
+        }
+    }
+
     fun getPriorInterviewList() {}
 }
